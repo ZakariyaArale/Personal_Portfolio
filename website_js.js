f234bbbcc4ff8fa342cb576f3337f4f9e6fb7e38
ZakariyaArale <141786740+ZakariyaArale@users.noreply.github.com>
2025-08-28 13:42:38 -0400

    Update website_js.js

---
+++ website_js.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+const homeSection = document.querySelector(".tofade");
+setTimeout(() => {
+    homeSection.classList.add("fade-in");
+}, 50);
+});
